40633662e06720cf8670e3f2722b541eb33a782a
UserCardinot <lucas.cardinot@outlook.com>
2024-11-05 20:52:22 -0300

    correcao de carrinho para usar com context

--- grupo3react/src/pages/Carrinho.jsx
+++ grupo3react/src/pages/Carrinho.jsx
@@ -1,66 +1,147 @@
-import React, { useEffect, useState } from 'react';
-import './carrinho.css';
-import produtosData from '../api/api';
+import React, { useEffect, useState } from "react";
+import "./carrinho.css";
+import { useContext } from "react";
+import { GeralContext } from "../context/GeralContext";
 
 export function Carrinho() {
-    const [produtos, setProdutos] = useState([]);
-    const [carrinho, setCarrinho] = useState([]);
+  const { produtos, setProdutos, carrinho, setCarrinho } =
+    useContext(GeralContext);
 
-    useEffect(() => {
-        setProdutos(produtosData.produtos);
-    }, []);
-
-    const adicionarAoCarrinho = (produto) => {
-        setCarrinho([...carrinho, produto]);
-    };
-
-    return (
-        <main className="main">
-            <div className="imagem">
-                <img src="src/img/logo10.png" alt="" />
-            </div>
-            <div className="form">
-
-
-                <h1 className="titulo">Carrinho</h1>
-
-                <div className="form2">
-                    <div className="produtos-container">
-                        <h2 className="subtitulo">Produtos</h2>
-                        <ul className="lista">
-                            {produtos.map((produto) => (
-                                <li
-                                    key={produto.id}
-                                    onClick={() => adicionarAoCarrinho(produto)}
-                                    style={{ cursor: 'pointer', transition: 'transform 0.2s' }}
-                                    onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
-                                    onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
-                                >
-                                    <h3>{produto.nome}</h3>
-                                    <p>Descrição: {produto.descrição}</p>
-                                    <p>Preço: R$ {produto.preco.toLocaleString()}</p>
-                                    <p>Categoria: {produto.categoria}</p>
-                                    <p>Quantidade: {produto.quantidade}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-
-                    <div className="carrinho-container">
-                        <h2 className="subtitulo">Carrinho</h2>
-                        <ul className="lista">
-                            {carrinho.map((item, index) => (
-                                <li key={index}>
-                                    <h3>{item.nome}</h3>
-                                    <p>Preço: R$ {item.preco.toLocaleString()}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </main>
+  const removerDoCarrinho = (produtoId) => {
+    const produtoRemovido = carrinho.find((item) => item.id === produtoId);
+    setCarrinho(carrinho.filter((item) => item.id !== produtoId));
+    setProdutos(
+      produtos.map((item) =>
+        item.id === produtoId
+          ? {
+              ...item,
+              quantidade: item.quantidade + produtoRemovido.quantidade,
+            }
+          : item
+      )
     );
+  };
+
+  const atualizarQuantidade = (produtoId, novaQuantidade) => {
+    setCarrinho(
+      carrinho.map((item) =>
+        item.id === produtoId ? { ...item, quantidade: novaQuantidade } : item
+      )
+    );
+  };
+
+  const calcularTotal = () => {
+    return carrinho.reduce(
+      (acc, item) => acc + item.preco * item.quantidade,
+      0
+    );
+  };
+
+  const limparCarrinho = () => {
+    carrinho.forEach((item) => {
+      setProdutos((prevProdutos) =>
+        prevProdutos.map((produto) =>
+          produto.id === item.id
+            ? { ...produto, quantidade: produto.quantidade + item.quantidade }
+            : produto
+        )
+      );
+    });
+    setCarrinho([]);
+  };
+
+  const finalizarCompra = () => {
+    if (carrinho.length === 0) {
+      alert("Adicione produtos ao carrinho antes de finalizar a compra!");
+    } else {
+      alert("Compra finalizada com sucesso!");
+      limparCarrinho();
+    }
+  };
+
+  return (
+    <main className="main">
+      <div className="imagem">
+        <img src="src/img/logo10.png" alt="" />
+      </div>
+      <div className="form">
+        <div className="form2">
+          <div className="carrinho-container">
+            <h2 className="subtitulo">Carrinho</h2>
+            <ul className="lista">
+              {carrinho.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => removerDoCarrinho(item.id)}
+                  style={{
+                    cursor: "pointer",
+                    transition: "background-color 0.2s",
+                  }}
+                  onMouseEnter={(e) =>
+                    (e.currentTarget.style.transform = "scale(1.05)")
+                  }
+                  onMouseLeave={(e) =>
+                    (e.currentTarget.style.transform = "scale(1)")
+                  }
+                >
+                  <div className="item-info">
+                    <h3>{item.nome}</h3>
+                    <p>Preço: R$ {item.preco.toLocaleString()}</p>
+                    <p>
+                      Total: R${" "}
+                      {(item.preco * item.quantidade).toLocaleString()}
+                    </p>
+                  </div>
+                  <div className="quantidade-container">
+                    <input
+                      type="number"
+                      min="1"
+                      value={item.quantidade}
+                      onClick={(e) => e.stopPropagation()}
+                      onChange={(e) =>
+                        atualizarQuantidade(item.id, parseInt(e.target.value))
+                      }
+                      style={{ width: "50px", marginTop: "5px" }}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="total-container">
+              <h3>Total: R$ {calcularTotal().toLocaleString()}</h3>
+            </div>
+            <button
+              className="limpar-carrinho"
+              onClick={limparCarrinho}
+              style={{ cursor: "pointer", transition: "transform 0.2s" }}
+              onMouseEnter={(e) =>
+                (e.currentTarget.style.transform = "scale(1.05)")
+              }
+              onMouseLeave={(e) =>
+                (e.currentTarget.style.transform = "scale(1)")
+              }
+            >
+              Limpar Carrinho
+            </button>
+
+            <button
+              className="finalizar-compra"
+              onClick={finalizarCompra}
+              style={{ cursor: "pointer", transition: "transform 0.2s" }}
+              onMouseEnter={(e) =>
+                (e.currentTarget.style.transform = "scale(1.05)")
+              }
+              onMouseLeave={(e) =>
+                (e.currentTarget.style.transform = "scale(1)")
+              }
+            >
+              Finalizar Compra
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Carrinho;
