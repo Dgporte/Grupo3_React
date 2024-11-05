9b5322918367d332bc806a119c6b4afe3e28f93f
João Gabriel Wermelinger <joaocfw7@gmail.com>
2024-11-04 22:27:57 -0300

    Login

--- grupo3react/src/App.jsx
+++ grupo3react/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-
 
 import './App.css'
 import { TelaDeCadastro } from './pages/TelaDeCadastro'
+import { Login } from "./pages/Login"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <BrowserRouter>
     <Switch>
       <Route exact path='/TelaDeCadastro' component={TelaDeCadastro}/>
+      <Route exact path="/Login" component={Login}/>
     </Switch>
     </BrowserRouter>
 
