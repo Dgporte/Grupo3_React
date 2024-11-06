9c4a7f491b7af747353fece874a25d8c57f7aca3
Dgporte <degevg1898@icloud.com>
2024-11-06 16:34:43 -0300

    algumas alteraçoes

--- grupo3react/src/App.jsx
+++ grupo3react/src/App.jsx
@@ -7,7 +7,6 @@ import { TelaDeCadastro } from './pages/Login/TelaDeCadastro'
 import Produto from './pages/Produtos/Produto'
 import CategoriaDetalhe from "./pages/Produtos/CategoriaDetalhe";
 import { Carrinho } from './pages/Carrinho/Carrinho'
-import { FinalCompra } from './pages/Finalizar/FinalCompra'
 import { Login } from "./pages/Login/Login"
 import Navbar from './components/Navbar'
 import { Home } from './pages/Home/home';
@@ -24,7 +23,6 @@ function App() {
       <Route exact path='/Produto' component={Produto} />
       <Route path='/categoria/:categoria' component={CategoriaDetalhe} />
       <Route exact path='/TelaDeCadastro' component={TelaDeCadastro}/>    
-      <Route exact path='/FinalCompra' component={FinalCompra}/>
       <Route exact path="/Login" component={Login}/>
       <Route exact path='/Carrinho' component={Carrinho}/>
     </Switch>
